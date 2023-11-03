4efcbb95625c657b5c291baf46c73bf819f886b7
KKamjangE <ajm980718@gmail.com>
2023-11-03 17:19:10 +0900

    Feat: Home 페이지 구현

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import Button from "./../components/Button";
+import home from "../api/home";
+
 const Home = () => {
-    return <h1>hello</h1>;
+    const [page, setPage] = useState(1);
+    const [perPage, setPerPage] = useState(10);
+
+    const onHandleClickGetCenters = async () => {
+        const { data } = await home.getCenters(page, perPage);
+        console.log(data);
+    };
+
+    return (
+        <div className="flex gap-5 items-end rounded">
+            <div className="flex flex-col items-start gap-2">
+                <label htmlFor="page" className="text-xs">
+                    Page
+                </label>
+                <input
+                    type="text"
+                    id="page"
+                    value={page}
+                    onChange={(e) => setPage(e.target.value)}
+                    className="border-solid border-2 rounded w-28 px-2 py-1 text-sm"
+                />
+            </div>
+            <div className="flex flex-col items-start gap-2">
+                <label htmlFor="page" className="text-xs">
+                    PerPage
+                </label>
+                <input
+                    type="text"
+                    id="page"
+                    value={perPage}
+                    onChange={(e) => setPerPage(e.target.value)}
+                    className="border-solid border-2 rounded w-28 px-2 py-1 text-sm"
+                />
+            </div>
+            <Button text={"조회"} clickEvent={onHandleClickGetCenters} />
+        </div>
+    );
 };
 
 export default Home;
