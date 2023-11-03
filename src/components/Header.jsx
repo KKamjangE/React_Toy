d3549dbe88cde8d1738c013a64659da360078ee1
KKamjangE <ajm980718@gmail.com>
2023-11-03 17:19:24 +0900

    Feat: header 페이지 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return <div></div>
-}
+    return (
+        <div className="flex justify-center items-center gap-5 p-4 shadow-lg bg-white sticky top-0">
+            <h1>Toy Project</h1>
+            <Link
+                to="/"
+                className="text-sm text-teal-700 hover:text-teal-500 transition"
+            >
+                Home
+            </Link>
+            <Link
+                to="/list"
+                className="text-sm text-teal-700 hover:text-teal-500 transition"
+            >
+                List
+            </Link>
+            <Link
+                to="/sign"
+                className="text-sm text-teal-700 hover:text-teal-500 transition"
+            >
+                Sign
+            </Link>
+        </div>
+    );
+};
 
 export default Header;
