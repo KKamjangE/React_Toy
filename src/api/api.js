d33b629f4d755d8c201b6afd44fde821fa801213
KKamjangE <ajm980718@gmail.com>
2023-11-03 17:18:15 +0900

    Feat: api path 관리 파일 생성

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+export const BASE_URL = "http://localhost:8080"; // 백엔드 base url
+
+export const POTAL_URL = "https://api.odcloud.kr/api/15077586/v1"; // 공공 데이터 포털 base url
+
+// endPoint 객체
+export const API_PATH = {
+    GET_CENTERS: (page, perPage) =>
+        `/centers?page=${page}&perPage=${perPage}&serviceKey=${
+            import.meta.env.VITE_POTAL_SECRET_KEY
+        }`,
+    POST_CENTER: "/",
+};
