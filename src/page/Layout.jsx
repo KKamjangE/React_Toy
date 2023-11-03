625cc05afeab963871073266ac3561797e750fdc
KKamjangE <ajm980718@gmail.com>
2023-11-03 15:26:20 +0900

    Feat: 레이아웃 변경

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -3,7 +3,9 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="flex justify-center mt-10">
-            <Outlet />
+            <section className="px-12 py-5 bg-white shadow-lg">
+                <Outlet />
+            </section>
         </div>
     );
 };
