237ea9a66e28765156c205caf082f86f6e6dc729
KKamjangE <ajm980718@gmail.com>
2023-11-03 13:28:22 +0900

    Feat: 공통 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Header from "./components/Header";
 import Home from "./page/Home";
 import List from "./page/List";
 import Sign from "./page/Sign";
+import Layout from "./page/Layout";
 
 function App() {
     return (
         <>
             <Header />
             <Routes>
-                <Route index element={<Home />} />
-                <Route path="/list" element={<List />} />
-                <Route path="/sign" element={<Sign />} />
+                <Route element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="/list" element={<List />} />
+                    <Route path="/sign" element={<Sign />} />
+                </Route>
             </Routes>
         </>
     );
