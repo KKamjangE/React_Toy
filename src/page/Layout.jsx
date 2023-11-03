b6a771b2103c68e79f31843015c1ced7604c3fd8
KKamjangE <ajm980718@gmail.com>
2023-11-03 17:53:22 +0900

    Design: 레이아웃 수정

--- src/page/Layout.jsx
+++ src/page/Layout.jsx
@@ -3,9 +3,7 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
     return (
         <div className="flex justify-center mt-10">
-            <section className="px-12 py-5 bg-white shadow-lg">
-                <Outlet />
-            </section>
+            <Outlet />
         </div>
     );
 };
