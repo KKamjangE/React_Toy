17b220ef9f52eb4dd9e9ee7d61760cc2a3e957c8
KKamjangE <ajm980718@gmail.com>
2023-11-03 17:18:41 +0900

    Feat: axios 요청 베이스 구현

---
+++ src/api/axios.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import qs from "qs";
+import { BASE_URL, POTAL_URL } from "./api";
+
+// axios 기본 설정
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    withCredentials: true,
+});
+
+// 데이터 포털 요청 기본 설정
+export const axiosPotalInstance = axios.create({
+    baseURL: POTAL_URL,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+// endPoint와 data를 쿼리스트링으로 변환
+const generateQueryEndPoint = (endPoint, data) => {
+    const queryString = qs.stringify(data, {
+        addQueryPrefix: true,
+    });
+
+    return `${endPoint}${queryString}`;
+};
+
+// 요청 에러 핸들러
+const requestErrorHandler = (err) => {
+    return Promise.reject(err);
+};
+
+// 응답 에러 핸들러
+const responseErrorHandler = (err) => {
+    return Promise.reject(err);
+};
+
+// axios 인터셉터로 에러 핸들링
+axiosInstance.interceptors.request.use((err) => requestErrorHandler(err));
+axiosInstance.interceptors.response.use((err) => responseErrorHandler(err));
+
+// 공통 get 요청
+export const getAPI = ({ endPoint, data, axiosOption }) => {
+    return axiosInstance.get(
+        data ? generateQueryEndPoint(endPoint, data) : endPoint,
+        axiosOption
+    );
+};
+
+// 공통 post 요청
+export const postAPI = ({ endPoint, data, axiosOption }) => {
+    return axiosInstance.post(endPoint, data, axiosOption);
+};
+
+// 공공 데이터 포털 get 요청
+export const getPotalAPI = ({ endPoint, data, axiosOption }) => {
+    return axiosPotalInstance.get(
+        data ? generateQueryEndPoint(endPoint, data) : endPoint,
+        axiosOption
+    );
+};
