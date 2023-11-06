11bdf9794f42266735d563e9ea4cbc373ef544ec
KKamjangE <ajm980718@gmail.com>
2023-11-06 16:18:55 +0900

    Feat: Center 관련 API Path 생성

--- src/api/api.js
+++ src/api/api.js
@@ -4,9 +4,11 @@ export const POTAL_URL = "https://api.odcloud.kr/api/15077586/v1"; // 공공 데
 
 // endPoint 객체
 export const API_PATH = {
-    GET_CENTERS: (page, perPage) =>
+    GET_SEARCH_CENTERS: (page, perPage) =>
         `/centers?page=${page}&perPage=${perPage}&serviceKey=${
             import.meta.env.VITE_POTAL_SECRET_KEY
         }`,
-    POST_CENTER: "/",
+    POST_CENTER: "/center",
+    GET_CENTERS: "/center",
+    DELETE_CENTER: (idx) => `/center?idx=${idx}`,
 };
