5890057303387a2a04064f7c5bc9cc5855fc46aa
KKamjangE <ajm980718@gmail.com>
2023-11-06 16:48:03 +0900

    Feat: zustand persist middleware 구현

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,15 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useCentersStore = create((set) => ({
-    centers: null,
-    setCenters: (newCenters) => {
-        set({ centers: newCenters });
-    },
-}));
+export const useCentersStore = create(
+    persist(
+        (set) => ({
+            centers: null,
+            setCenters: (newCenters) => set({ centers: newCenters }),
+        }),
+        {
+            name: "center-storage",
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+);
