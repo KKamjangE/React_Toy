2b791dd06663d591563942eb4dbc822fc2efaa3b
KKamjangE <ajm980718@gmail.com>
2023-11-06 16:21:21 +0900

    Feat: List 페이지 구현

--- src/page/List.jsx
+++ src/page/List.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import centerAPI from "../api/centerAPI";
+import Center from "../components/list/Center";
+
 const List = () => {
-    return <div>List</div>;
+    const [centers, setCenters] = useState();
+
+    useEffect(() => {
+        const { data } = centerAPI.getCenters();
+        if (data) {
+            setCenters(data);
+        }
+    }, []);
+
+    return (
+        <section>
+            {centers ? (
+                centers.map((center) => (
+                    <Center key={center.id} center={center} />
+                ))
+            ) : (
+                <div>
+                    <p>저장된 데이터가 없습니다.</p>
+                </div>
+            )}
+        </section>
+    );
 };
 
 export default List;
