63525b50e86a258f5f1214f53c8c938c4543f876
KKamjangE <ajm980718@gmail.com>
2023-11-10 17:05:22 +0900

    Feat: sign in 후 Nav에 닉네임 출력

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useMemberStore } from "../../store/store";
 
 const Header = () => {
+    const memberInfo = useMemberStore((state) => state.memberInfo);
     return (
         <div className="flex justify-center items-center gap-5 p-4 shadow-lg bg-white sticky top-0">
             <h1>Toy Project</h1>
@@ -16,12 +18,18 @@ const Header = () => {
             >
                 List
             </Link>
-            <Link
-                to="/sign"
-                className="text-sm text-teal-700 hover:text-teal-500 transition"
-            >
-                Sign
-            </Link>
+            {memberInfo.memberName ? (
+                <span className="text-sm text-teal-500 font-bold">
+                    {memberInfo.memberName}
+                </span>
+            ) : (
+                <Link
+                    to="/sign"
+                    className="text-sm text-teal-700 hover:text-teal-500 transition"
+                >
+                    Sign
+                </Link>
+            )}
         </div>
     );
 };
