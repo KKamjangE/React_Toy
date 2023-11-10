7e96d878fe4498dd68470f896bd8141f0addd759
KKamjangE <ajm980718@gmail.com>
2023-11-10 10:43:50 +0900

    Fix: sign 관련 api Promise 반환하도록 변경

--- src/api/signAPI/index.js
+++ src/api/signAPI/index.js
@@ -2,11 +2,11 @@ import { API_PATH } from "../api";
 import { postAPI } from "../axios";
 
 const postSignIn = (data) => {
-    postAPI({ endPoint: API_PATH.POST_SIGN_IN, data });
+    return postAPI({ endPoint: API_PATH.POST_SIGN_IN, data });
 };
 
 const postSignUp = (data) => {
-    postAPI({ endPoint: API_PATH.POST_SIGN_UP, data });
+    return postAPI({ endPoint: API_PATH.POST_SIGN_UP, data });
 };
 
 export default {
