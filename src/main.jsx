e5c13ed0a55559a84e7ed67e17732e2f48bf078d
KKamjangE <ajm980718@gmail.com>
2023-11-10 10:56:12 +0900

    Feat: QueryClient 캐시 기본 설정

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 100000,
+            gcTime: 600000,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
