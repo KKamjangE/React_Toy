2760271ead76ee10d93dc50a3dfae3774646164e
KKamjangE <ajm980718@gmail.com>
2023-11-10 16:45:33 +0900

    Fix: QueryClient 기본 옵션 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 100000,
-            gcTime: 600000,
-        },
-    },
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
