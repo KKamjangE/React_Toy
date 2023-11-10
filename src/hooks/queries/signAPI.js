d16423a82c367fd827f94b222d5586262af90992
KKamjangE <ajm980718@gmail.com>
2023-11-10 14:33:19 +0900

    Feat: 회원가입 응답 처리

--- src/hooks/queries/signAPI.js
+++ src/hooks/queries/signAPI.js
@@ -1,10 +1,19 @@
 import { useMutation } from "@tanstack/react-query";
 import signAPI from "../../api/signAPI";
-
+import { useSignStore } from "../../store/store";
 export const usePostSignIn = () => {
     return useMutation({ mutationFn: signAPI.postSignIn });
 };
 
 export const usePostSignUp = () => {
-    return useMutation({ mutationFn: signAPI.postSignUp });
+    const { setIsSignIn } = useSignStore();
+    return useMutation({
+        mutationFn: signAPI.postSignUp,
+        onSuccess: (data) => {
+            alert(data.responseMessage);
+            if (data.statusCode === 200) {
+                setIsSignIn();
+            }
+        },
+    });
 };
