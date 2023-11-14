8cd230a36ae6df951efd9f0efca5fdbc68bceed2
KKamjangE <ajm980718@gmail.com>
2023-11-14 18:10:26 +0900

    Feat: main에서 처리하던 provider App에서 처리, react query 공통 에러 처리

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,48 @@ import Sign from "./page/Sign";
 import Layout from "./components/common/Layout";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { BrowserRouter } from "react-router-dom";
+import useApiHandler from "./hooks/queries/useApiError";
+import InternalServerError from "./page/InternalServerError";
 
 function App() {
+    const { handleError } = useApiHandler();
+
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                throwOnError: handleError,
+            },
+            mutations: {
+                onError: handleError,
+            },
+        },
+    });
+
     return (
-        <>
-            <Header />
-            <Routes>
-                <Route element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route element={<PrivateRoute />}>
-                        <Route path="/list" element={<List />} />
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route element={<PrivateRoute />}>
+                            <Route path="/list" element={<List />} />
+                        </Route>
+                        <Route element={<PublicRoute />}>
+                            <Route path="/sign" element={<Sign />} />
+                            <Route
+                                path="/500"
+                                element={<InternalServerError />}
+                            />
+                        </Route>
                     </Route>
-                    <Route element={<PublicRoute />}>
-                        <Route path="/sign" element={<Sign />} />
-                    </Route>
-                </Route>
-            </Routes>
-        </>
+                </Routes>
+            </BrowserRouter>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-            <App />
-            <ReactQueryDevtools initialIsOpen={false} />
-        </BrowserRouter>
-    </QueryClientProvider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
