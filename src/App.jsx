1a2c0ee5c4c87a631e716db06b1a618112af50e8
KKamjangE <ajm980718@gmail.com>
2023-11-17 15:31:15 +0900

    Rename: useApiError 파일 위치 이동

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import {
     QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import useApiError from "./hooks/queries/useApiError";
+import useApiError from "./hooks/useApiError";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
