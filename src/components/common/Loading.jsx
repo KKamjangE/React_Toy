2d132756c6154dd21f5a293929e3b1edbfd72dc7
KKamjangE <ajm980718@gmail.com>
2023-11-21 17:19:00 +0900

    Design: 로딩 컴포넌트 스타일링

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -1,6 +1,6 @@
 const Loading = () => {
     return (
-        <div className="flex justify-center">
+        <div className="flex justify-center py-10">
             <p>데이터를 불러오는 중 입니다.</p>
         </div>
     );
