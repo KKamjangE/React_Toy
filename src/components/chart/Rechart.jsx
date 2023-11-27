5cd16b44b3d57a878bbc4b8817b0eb59e866a835
KKamjangE <ajm980718@gmail.com>
2023-11-27 17:53:34 +0900

    Feat: recharts 라이브러리를 사용한 컴포넌트

---
+++ src/components/chart/Rechart.jsx
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import { Cell, LabelList, Legend, Pie, PieChart, Tooltip } from "recharts";
+
+const Rechart = ({ data }) => {
+    const colors = {
+        0: "#e9c19f",
+        1: "#f47560",
+    };
+
+    const [activeName, setActiveName] = useState(null);
+
+    const countObject = data.reduce((result, item) => {
+        result[item.centerType] = (result[item.centerType] || 0) + 1;
+        return result;
+    }, {});
+
+    const chartData = Object.keys(countObject).map((key) => ({
+        name: key,
+        value: countObject[key],
+        select: false,
+    }));
+
+    chartData.sort((a, b) => a.value - b.value);
+
+    return (
+        <PieChart width={400} height={350}>
+            <Pie
+                data={chartData}
+                dataKey={"value"}
+                nameKey={"name"}
+                isAnimationActive={false}
+                label={(entry) => entry.name}
+            >
+                {chartData.map((item, index) => (
+                    <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index]}
+                        fillOpacity={
+                            activeName && activeName !== item.name && 0.5
+                        }
+                    />
+                ))}
+                <LabelList dataKey={"value"} />
+            </Pie>
+            <Tooltip />
+            <Legend
+                onMouseEnter={(prop) => setActiveName(prop.value)}
+                onMouseLeave={() => setActiveName(null)}
+            />
+        </PieChart>
+    );
+};
+
+Rechart.propTypes = {
+    data: PropTypes.array.isRequired,
+};
+
+export default Rechart;
