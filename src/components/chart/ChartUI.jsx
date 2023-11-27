2232eef3cd1912d2d58287c69a6f835486b638f5
KKamjangE <ajm980718@gmail.com>
2023-11-27 17:53:04 +0900

    Feat: Chart 라이브러리 별로 분리

--- src/components/chart/ChartUI.jsx
+++ src/components/chart/ChartUI.jsx
@@ -1,85 +1,23 @@
-import { ResponsivePie } from "@nivo/pie";
 import { useGetCenters } from "../../hooks/queries/centerAPI";
+import { useState } from "react";
+import Button from "./../common/Button";
+import Nivo from "./Nivo";
+import Rechart from "./Rechart";
 
 const ChartUI = () => {
     const { data } = useGetCenters();
-    const testData = [
-        {
-            id: "lisp",
-            label: "lisp",
-            value: 288,
-            color: "hsl(322, 70%, 50%)",
-        },
-        {
-            id: "php",
-            label: "php",
-            value: 51,
-            color: "hsl(250, 70%, 50%)",
-        },
-        {
-            id: "erlang",
-            label: "erlang",
-            value: 345,
-            color: "hsl(5, 70%, 50%)",
-        },
-        {
-            id: "css",
-            label: "css",
-            value: 28,
-            color: "hsl(213, 70%, 50%)",
-        },
-        {
-            id: "go",
-            label: "go",
-            value: 281,
-            color: "hsl(13, 70%, 50%)",
-        },
-    ];
 
-    data.data.forEach((item) => {
-        console.log(item);
-    });
+    const [change, setChange] = useState(false);
+
+    const changeChart = () => {
+        setChange((prev) => !prev);
+    };
 
     return (
-        <ResponsivePie
-            data={testData}
-            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-            sortByValue={true}
-            colors={{ scheme: "dark2" }}
-            activeOuterRadiusOffset={8}
-            arcLinkLabelsTextColor="#fff"
-            arcLinkLabelsThickness={2}
-            arcLinkLabelsColor={{ from: "color" }}
-            arcLabelsTextColor={{
-                from: "color",
-                modifiers: [["brighter", "3"]],
-            }}
-            legends={[
-                {
-                    anchor: "bottom",
-                    direction: "row",
-                    justify: false,
-                    translateX: 0,
-                    translateY: 56,
-                    itemsSpacing: 0,
-                    itemWidth: 100,
-                    itemHeight: 18,
-                    itemTextColor: "#999",
-                    itemDirection: "left-to-right",
-                    itemOpacity: 1,
-                    symbolSize: 18,
-                    symbolShape: "circle",
-                    effects: [
-                        {
-                            on: "hover",
-                            style: {
-                                itemTextColor: "#fff",
-                            },
-                        },
-                    ],
-                },
-            ]}
-        />
+        <section className="flex flex-col items-center w-1/3 h-96 rounded bg-gray-700 px-12 py-5 shadow-lg shadow-slate-950 text-black text-xs">
+            <Button text="Chart Change" clickEvent={changeChart} />
+            {change ? <Nivo data={data.data} /> : <Rechart data={data.data} />}
+        </section>
     );
 };
 
