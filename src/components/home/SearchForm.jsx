aa3f140688f1599e0462b7ff9406ef995d2eaa92
KKamjangE <ajm980718@gmail.com>
2023-11-29 10:46:26 +0900

    Feat: SearchFrom 컴포넌트 mui 적용

--- src/components/home/SearchForm.jsx
+++ src/components/home/SearchForm.jsx
@@ -1,47 +1,62 @@
 import { useQueryClient } from "@tanstack/react-query";
 import { useSearchStateStore } from "../../store/store";
-import Button from "../common/Button";
 import { queryKeys } from "../../contents/queryKeys";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    TextField,
+} from "@mui/material";
 
 const SearchForm = () => {
     const { page, perPage, setPage, setPerPage } = useSearchStateStore();
-
     const queryClient = useQueryClient();
 
     const handleSearch = () => {
         queryClient.fetchQuery({ queryKey: [queryKeys.getSearchCenters] });
     };
 
+    const handleEnter = (e) => {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    };
+
     return (
-        <div className="flex justify-between items-end gap-5 rounded bg-gray-700 shadow-lg shadow-slate-950 px-12 py-5 mb-5">
-            <div className="flex gap-5">
-                <div className="flex flex-col items-start gap-2">
-                    <label htmlFor="page" className="text-xs">
-                        Page
-                    </label>
-                    <input
-                        type="text"
-                        id="page"
-                        value={page}
-                        onChange={(e) => setPage(e.target.value)}
-                        className="border-solid border-2 rounded w-36 px-2 py-1 text-sm"
-                    />
-                </div>
-                <div className="flex flex-col items-start gap-2">
-                    <label htmlFor="page" className="text-xs">
-                        PerPage
-                    </label>
-                    <input
-                        type="text"
-                        id="page"
-                        value={perPage}
-                        onChange={(e) => setPerPage(e.target.value)}
-                        className="border-solid border-2 rounded w-36 px-2 py-1 text-sm"
-                    />
-                </div>
-            </div>
-            <Button text={"조회"} clickEvent={handleSearch} />
-        </div>
+        <Card elevation={6}>
+            <CardHeader title="코로나 예방접종센터 검색" />
+            <CardContent
+                sx={{ display: "flex", justifyContent: "space-around" }}
+            >
+                <TextField
+                    id="page"
+                    label="page"
+                    defaultValue={page}
+                    onChange={(e) => {
+                        setPage(e.target.value);
+                    }}
+                    onKeyDown={handleEnter}
+                    size="small"
+                    autoComplete="off"
+                />
+                <TextField
+                    id="perPage"
+                    label="perPage"
+                    defaultValue={perPage}
+                    onChange={(e) => setPerPage(e.target.value)}
+                    onKeyDown={handleEnter}
+                    size="small"
+                    autoComplete="off"
+                />
+            </CardContent>
+            <CardActions sx={{ justifyContent: "end" }}>
+                <Button variant="contained" onClick={handleSearch}>
+                    검색
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
 
--- src/components/home/SearchResult.jsx
+++ src/components/home/SearchResult.jsx
@@ -1,13 +1,36 @@
-import { useGetSearchCenters } from "../../hooks/queries/centerAPI";
+import {
+    useGetSearchCenters,
+    usePostCenter,
+} from "../../hooks/queries/centerAPI";
 import CenterItem from "../common/CenterItem";
 
 const SearchResult = () => {
     const { data } = useGetSearchCenters();
 
+    const { mutate } = usePostCenter();
+
+    const handleSaveCenter = (center) => {
+        mutate({
+            id: center.id,
+            address: center.address,
+            centerName: center.centerName,
+            updatedAt: center.updatedAt,
+            lat: center.lat,
+            lng: center.lng,
+            phoneNumber: center.phoneNumber,
+            centerType: center.centerType,
+        });
+    };
+
     return (
         <>
             {data.data.data.map((center) => (
-                <CenterItem key={center.id} center={center} />
+                <CenterItem
+                    key={center.id}
+                    center={center}
+                    clickEvent={handleSaveCenter}
+                    btnName={"저장"}
+                />
             ))}
         </>
     );
