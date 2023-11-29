6b2ff67d6dc4b2428408a190806956ee78fa6541
KKamjangE <ajm980718@gmail.com>
2023-11-29 10:45:53 +0900

    Refactor: centerItem 컴포넌트 mui 적용 및 의존성 관리

--- src/components/common/CenterItem.jsx
+++ src/components/common/CenterItem.jsx
@@ -1,61 +1,58 @@
 import PropTypes from "prop-types";
-import Button from "./Button";
-import { useDeleteCenter, usePostCenter } from "../../hooks/queries/centerAPI";
 import { useMemberStore } from "../../store/store";
 import { useNavigate } from "react-router-dom";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    Typography,
+} from "@mui/material";
+import { toast } from "react-toastify";
 
-const CenterItem = ({ center }) => {
-    const { mutate: postMutate } = usePostCenter();
-    const { mutate: deleteMutate } = useDeleteCenter();
+const CenterItem = ({ center, clickEvent, btnName }) => {
     const isSignIn = useMemberStore((state) => state.isSignIn);
     const navigater = useNavigate();
 
-    const handleSaveCenter = () => {
-        postMutate({
-            id: center.id,
-            address: center.address,
-            centerName: center.centerName,
-            updatedAt: center.updatedAt,
-            lat: center.lat,
-            lng: center.lng,
-            phoneNumber: center.phoneNumber,
-            centerType: center.centerType,
-        });
-    };
-
-    const handleDeleteCenter = () => {
-        deleteMutate(center.id);
-    };
-
     const handleClick = () => {
         if (isSignIn) {
-            location.pathname === "/"
-                ? handleSaveCenter()
-                : handleDeleteCenter();
+            clickEvent(center);
         } else {
-            alert("로그인이 필요한 기능입니다.");
+            toast.error("로그인이 필요한 기능입니다.");
             navigater("/sign");
         }
     };
 
     return (
-        <article className="flex items-end justify-between gap-4 bg-gray-700 px-12 py-5 mb-5 shadow-lg shadow-slate-950 rounded">
-            <div>
-                <h3>{center.centerName}</h3>
-                <p className="text-sm">{center.address}</p>
-                <span className="text-xs">Last update: {center.updatedAt}</span>
-            </div>
-            <Button
-                text={location.pathname === "/" ? "저장" : "삭제"}
-                clickEvent={handleClick}
-                color={location.pathname === "/" ? undefined : "red"}
-            />
-        </article>
+        <Card
+            elevation={6}
+            sx={{
+                display: "flex",
+                alignItems: "end",
+                justifyContent: "space-between",
+                marginY: 3,
+            }}
+        >
+            <CardContent>
+                <Typography variant="h6">{center.centerName}</Typography>
+                <Typography variant="body1">{center.address}</Typography>
+                <Typography variant="caption">
+                    Last update: {center.updatedAt}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button variant="contained" onClick={handleClick}>
+                    {btnName}
+                </Button>
+            </CardActions>
+        </Card>
     );
 };
 
 CenterItem.propTypes = {
     center: PropTypes.object.isRequired,
+    clickEvent: PropTypes.func.isRequired,
+    btnName: PropTypes.string.isRequired,
 };
 
 export default CenterItem;
