4fdf560e9007588420d30d8eaaaec2f255c767c4
KKamjangE <ajm980718@gmail.com>
2023-11-29 10:45:08 +0900

    Feat: 테마 설정 추가

--- src/theme.jsx
+++ src/theme.jsx
@@ -4,6 +4,15 @@ const theme = createTheme({
     palette: {
         mode: "dark",
     },
+    components: {
+        MuiCard: {
+            styleOverrides: {
+                root: {
+                    padding: 20,
+                },
+            },
+        },
+    },
 });
 
 export default theme;
