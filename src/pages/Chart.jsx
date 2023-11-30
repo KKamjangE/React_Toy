8d660237a596d42383a465fe0b4f92bd34f3b3d5
KKamjangE <ajm980718@gmail.com>
2023-11-30 18:35:13 +0900

    Feat: Suspense fallback 컴포넌트 스켈레톤으로 변경

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -3,14 +3,14 @@ import ChartUI from "./../components/chart/ChartUI";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import FallbackUI from "./../components/common/FallbackUI";
-import Loading from "./../components/common/Loading";
+import SkeletonChart from "../components/skeleton/SkeletonChart";
 
 const Chart = () => {
     return (
         <QueryErrorResetBoundary>
             {({ reset }) => (
                 <ErrorBoundary onReset={reset} fallbackRender={FallbackUI}>
-                    <Suspense fallback={<Loading />}>
+                    <Suspense fallback={<SkeletonChart />}>
                         <ChartUI />
                     </Suspense>
                 </ErrorBoundary>
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,23 +1,21 @@
 import { QueryErrorResetBoundary } from "@tanstack/react-query";
 import MapUI from "./../components/map/MapUI";
 import { Suspense } from "react";
-import Loading from "../components/common/Loading";
 import { ErrorBoundary } from "react-error-boundary";
 import FallbackUI from "../components/common/FallbackUI";
+import SkeletonMap from "../components/skeleton/SkeletonMap";
 
 const Map = () => {
     return (
-        <>
-            <QueryErrorResetBoundary>
-                {({ reset }) => (
-                    <ErrorBoundary onReset={reset} fallbackRender={FallbackUI}>
-                        <Suspense fallback={<Loading />}>
-                            <MapUI />
-                        </Suspense>
-                    </ErrorBoundary>
-                )}
-            </QueryErrorResetBoundary>
-        </>
+        <QueryErrorResetBoundary>
+            {({ reset }) => (
+                <ErrorBoundary onReset={reset} fallbackRender={FallbackUI}>
+                    <Suspense fallback={<SkeletonMap />}>
+                        <MapUI />
+                    </Suspense>
+                </ErrorBoundary>
+            )}
+        </QueryErrorResetBoundary>
     );
 };
 
