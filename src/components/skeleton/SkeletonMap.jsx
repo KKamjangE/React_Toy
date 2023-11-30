ec1e3de9444bcaab6d39e2129eccc537fc40e2e1
KKamjangE <ajm980718@gmail.com>
2023-11-30 18:34:25 +0900

    Feat: 지도 스켈레톤 구현

---
+++ src/components/skeleton/SkeletonMap.jsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@mui/material";
+
+const SkeletonMap = () => {
+    return (
+        <Skeleton variant="rectangular" sx={{ width: "100%", height: 650 }} />
+    );
+};
+
+export default SkeletonMap;
