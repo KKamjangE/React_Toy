dcaa7bed49e16d23cba8476e5b5074f13df7c2fe
KKamjangE <ajm980718@gmail.com>
2023-11-30 17:35:49 +0900

    Remove: 로딩 컴포넌트 삭제

--- src/components/common/Loading.jsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-    return (
-        <div className="flex justify-center py-10">
-            <p>데이터를 불러오는 중 입니다.</p>
-        </div>
-    );
-};
-
-export default Loading;
