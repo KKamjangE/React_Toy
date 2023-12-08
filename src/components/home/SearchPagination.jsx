8eedd1b90e6c206cd54c68e98b19f643a3c5be79
KKamjangE <ajm980718@gmail.com>
2023-12-08 14:59:46 +0900

    Feat: 이전, 다음 페이지 데이터 prefetchQuery 구현

--- src/components/home/SearchPagination.jsx
+++ src/components/home/SearchPagination.jsx
@@ -1,12 +1,36 @@
+import centerAPI from "@/api/centerAPI";
+import { queryKeys } from "@/contents/queryKeys";
 import { useSearchStateStore } from "@/store/store";
 import { Pagination } from "@mui/material";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const SearchPagination = () => {
-    const { page, maxPage, setPage } = useSearchStateStore((state) => ({
-        page: state.page,
-        maxPage: state.maxPage,
-        setPage: state.setPage,
-    }));
+    const { page, perPage, maxPage, setPage } = useSearchStateStore(
+        (state) => ({
+            page: state.page,
+            perPage: state.perPage,
+            maxPage: state.maxPage,
+            setPage: state.setPage,
+        }),
+    );
+    const queryClient = useQueryClient();
+    useEffect(() => {
+        if (page < maxPage) {
+            // 다음 페이지에 대한 데이터 미리 캐싱
+            queryClient.prefetchQuery({
+                queryKey: [queryKeys.getSearchCenters, page + 1, perPage],
+                queryFn: () => centerAPI.getSearchCenters(page + 1, perPage),
+            });
+        }
+        if (1 < page) {
+            // 이전 페이지에 대한 데이터 미리 캐싱
+            queryClient.prefetchQuery({
+                queryKey: [queryKeys.getSearchCenters, page - 1, perPage],
+                queryFn: () => centerAPI.getSearchCenters(page - 1, perPage),
+            });
+        }
+    }, [page, perPage, queryClient, maxPage]);
 
     return (
         <Pagination
