3ceb536dd53de843f1e083c742762ea92066f25e
KKamjangE <ajm980718@gmail.com>
2023-12-08 14:41:39 +0900

    Refactor: 센터 검색 입력값 persist 제거

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,12 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useSearchStateStore = create(
-    persist(
-        (set) => ({
-            page: 1,
-            perPage: 10,
-            setPage: (newPage) => set({ page: newPage }),
-            setPerPage: (newPerPage) => set({ perPage: newPerPage }),
-        }),
-        {
-            name: "search-storage",
-            storage: createJSONStorage(() => localStorage),
-        },
-    ),
-);
+export const useSearchStateStore = create((set) => ({
+    page: 1,
+    perPage: 10,
+    setPage: (newPage) => set({ page: newPage }),
+    setPerPage: (newPerPage) => set({ perPage: newPerPage }),
+}));
 
 export const useSignStore = create(
     persist(
